370c8fa8db51a93dfab99d12a9680fe23977ca4b
Ulises Rodriguez <uliisesrodriguez809@gmail.com>
2023-09-07 16:10:24 -0300

    primer commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const NavBar = () => {
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li>a</li>
+                    <li>b</li>
+                    <li>c</li>
+                    <li>d</li>
+                    <li>f</li>
+                </ul>
+            </nav>
+        </>
+    )
+}
